3af12eaf638f06f72b96243cf240000c9e2562df
[C[B[A[A[A[A[D[C[C[C[C[C[C[C[C[C[CiLi01[D[D[C[C[C[B[D[D[D[D[D[D[D[D[D[D[Cilinca.pricop100@gmail.com[C[B[C[C[C[C[C[C[C <ilinca.pricop100@gmail.com>
2023-10-02 18:31:45 +0100

    wip

--- script.js
+++ script.js
@@ -46,13 +46,34 @@ function displayImages() {
 }
 
 // nav-bar
-const navToggle = document.querySelector("#menu-btn");
-const nav = document.querySelector("nav");
+// Select the button that will expand the navigation
+const navExpandButton = document.querySelector('#nav-expand-button')
 
-navToggle.addEventListener("click", () => {
-  nav.classList.toggle("open")
-}
-)
+// select the element that is your navigation
+const nav = document.getElementById('main-nav')
+
+
+// create a function to tell the 
+// browser what to do when the button is clicked
+function handleClick() {
+    
+    // toggle a class of 'open' on the nav element, this will 
+    // change the visual style to `display: flex;` from `display: none;`
+    nav.classList.toggle('open')
+    
+
+    // get the state of the 'aria-expanded' attribute
+    const expandedState = nav.getAttribute('aria-expanded')
+        
+    // set the attribute of 'aria-expanded' to the opposite of what it currently is
+    nav.setAttribute('aria-expanded', !eval(expandedState))
+
+};
+
+
+// tell the browser to listen for a click event 
+// and to run "handleClick" when it is clicked
+navExpandButton.addEventListener('click', handleClick)
 
 
 
