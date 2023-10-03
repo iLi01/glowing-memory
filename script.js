63cd0f47675276184b5089e0441622d3d8b4abc2
[C[B[A[A[A[A[D[C[C[C[C[C[C[C[C[C[CiLi01[D[D[C[C[C[B[D[D[D[D[D[D[D[D[D[D[Cilinca.pricop100@gmail.com[C[B[C[C[C[C[C[C[C <ilinca.pricop100@gmail.com>
2023-10-03 18:10:40 +0100

    nav bar changes

--- script.js
+++ script.js
@@ -37,6 +37,7 @@ function displayImages() {
     images[i].style.display = "none";
   }
   index++;
+  
   if (index > images.length) {
     index = 1;
   }
@@ -46,34 +47,9 @@ function displayImages() {
 }
 
 // nav-bar
-// Select the button that will expand the navigation
-const navExpandButton = document.querySelector('#nav-expand-button')
+const button = document.getElementById("nav-expand-button");
+const nav = document.getElementById("main-nav");
 
-// select the element that is your navigation
-const nav = document.getElementById('main-nav')
-
-
-// create a function to tell the 
-// browser what to do when the button is clicked
-function handleClick() {
-    
-    // toggle a class of 'open' on the nav element, this will 
-    // change the visual style to `display: flex;` from `display: none;`
-    nav.classList.toggle('open')
-    
-
-    // get the state of the 'aria-expanded' attribute
-    const expandedState = nav.getAttribute('aria-expanded')
-        
-    // set the attribute of 'aria-expanded' to the opposite of what it currently is
-    nav.setAttribute('aria-expanded', !eval(expandedState))
-
-};
-
-
-// tell the browser to listen for a click event 
-// and to run "handleClick" when it is clicked
-navExpandButton.addEventListener('click', handleClick)
-
-
-
+button.addEventListener('click', () => {
+  nav.classList.toggle('open');
+});
