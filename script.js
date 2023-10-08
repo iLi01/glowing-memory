fb5e93ab7cea1dc44bf551be912e9aecf2bcb8c6
[C[B[A[A[A[A[D[C[C[C[C[C[C[C[C[C[CiLi01[D[D[C[C[C[B[D[D[D[D[D[D[D[D[D[D[Cilinca.pricop100@gmail.com[C[B[C[C[C[C[C[C[C <ilinca.pricop100@gmail.com>
2023-10-08 17:58:29 +0100

    wip

--- script.js
+++ script.js
@@ -19,8 +19,8 @@ span.onclick = function() {
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
+window.onclick = function(e) {
+  if (e.target == modal) {
     modal.style.display = "none";
   }
 }
@@ -53,4 +53,3 @@ const nav = document.getElementById("main-nav");
 button.addEventListener('click', () => {
   nav.classList.toggle('open');
 });
-
